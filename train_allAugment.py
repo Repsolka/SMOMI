1b1504b56766822bb06a2fdc80b8b6b29ccce516
Repsolka <61493441+Repsolka@users.noreply.github.com>
2020-04-18 20:31:35 +0300

    Update train_allAugment.py

--- train_allAugment.py
+++ train_allAugment.py
@@ -131,7 +131,7 @@ def main():
     model = build_model()
 
     model.compile(
-        optimizer=keras.optimizers.sgd(lr=0.00000000005, momentum=0.9),
+        optimizer=keras.optimizers.sgd(lr=0.00000000001, momentum=0.9),
         loss=tf.keras.losses.categorical_crossentropy,
         metrics=[tf.keras.metrics.categorical_accuracy],
         target_tensors=[train_labels]
